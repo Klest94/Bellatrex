f658334077ec28fb316a23b3e349a29b14914767
KlestDedja <43683592+KlestDedja@users.noreply.github.com>
2026-07-13 23:56:05 +0200

    fix synthax error

--- app/bellatrex/plot_tree_patch.py
+++ app/bellatrex/plot_tree_patch.py
@@ -380,13 +380,11 @@ class _MPLTreeExporter(_BaseTreeExporter):
         # validate
         if isinstance(precision, Integral):
             if precision < 0:
-                raise ValueError(f
-                    "'precision' should be greater or equal to 0."
-                    " Got {precision} instead.")
+                raise ValueError(
+                    "'precision' should be greater or equal to 0." f" Got {precision} instead."
                 )
         else:
             raise ValueError(f"'precision' should be an integer. Got {type(precision)} instead.")
-            )
 
         # The depth of each node for plotting with 'leaf' option
         self.ranks = {"leaves": []}
